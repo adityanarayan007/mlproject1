377e5db63673e8461ed3f2233c9169744b702c93
adityanarayan007 <adityam1729@gmail.com>
2024-04-27 21:27:30 +0530

    Setup and requirements commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+##with this we can create a package..
+
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    ##this function will return required list
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements =file_obj.readlines()
+        requirements =[req.replace("\n"," ")for req in requirements] 
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+    name='mlproject',
+    version='0.01',
+    author='Aditya',
+    author_email="adityam1729@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    #['pandas','numpy','seaborn']
+)
